c7e523e67caaaca2b1d62612922f04b2d5552b2e
danmuck <danmuck426@gmail.com>
2022-05-09 19:45:07 -0400

    init

---
+++ complex_nums.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+// Powers of j:
+//      j^1 = j
+//      j^2 = -1
+//      j^3 = (-1)j = -j
+//      j^4 = (-1)(-1) = 1
+//      j^5 = j^4j = 1j = j
+
+// Define a complex number in rectangular form (a + bj) where a and b are real numbers
+struct cmplx_abj {
+    int real_a;
+    int imag_b;
+};
+
+struct cmplx_abj add_two(int real_a, int imag_b) {
+    return cmplx_abj;
+};
+
+int main() {
+    printf("SupDawg\n");
+
+
+
+}
